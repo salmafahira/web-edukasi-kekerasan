22c035d46b75ba60e9030bfb6e1a313a29b4cb5b
Salma Fahira Azzahra <salmafahiraa05@gmail.com>
2026-01-04 06:15:32 +0700

    Memisahkan tampilan awal user dan admin

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Website Edukasi Kekerasan</title>
+</head>
+<body>
+    <h1>Selamat Datang di Website Edukasi Kekerasan</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/user', function () {
+    return view('user.home');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/profil', function () {
+    return view('user.profil');
+});
+
+/* ======================
+| ADMIN (WAJIB LOGIN)
+|======================*/
+Route::get('/admin/dashboard', function () {
+
+    if (!auth()->check() || auth()->user()->role !== 'admin') {
+        abort(403);
+    }
+
+    return view('admin.dashboard');
 });
